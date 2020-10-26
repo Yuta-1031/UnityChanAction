9e391e1db199f74672b16bd92eb829cc4815e8a8
高橋佑太 <yagisan555@icloud.com>
2020-10-26 19:57:54 +0900

    現状コミット

---
+++ Assets/Assets/Package/Concrete textures pack/CubeTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeTest : MonoBehaviour
+{
+    private float hp = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Sword")
+        {
+            Debug.Log("Hit");
+            this.hp -= 2;
+        }
+    }
+}
