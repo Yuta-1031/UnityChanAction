07f66bec4d83714700cd1fe6616ed70278d14f6d
高橋佑太 <yagisan555@icloud.com>
2020-12-05 23:42:58 +0900

    現状コミット

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
+    public GameObject player;
+    public GameObject player2;
     public float playerLife = 100;
     public float player2Life = 100;
+
+    private bool pl_Change = true;
 
     private void Awake()
     {
@@ -18,6 +22,39 @@ public class GameManager : MonoBehaviour
         else
         {
             Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(player);
+        DontDestroyOnLoad(player2);
+
+        player.SetActive(true);
+        player2.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (pl_Change == true)
+            {
+                player2.transform.position = player.transform.position;
+                player2.transform.rotation = player.transform.rotation;
+
+                player.SetActive(false);
+                player2.SetActive(true);
+
+                pl_Change = false;
+            }
+            else if (pl_Change == false)
+            {
+                player.transform.position = player2.transform.position;
+                player.transform.rotation = player2.transform.rotation;
+
+                player.SetActive(true);
+                player2.SetActive(false);
+
+                pl_Change = true;
+            }
         }
     }
 }
