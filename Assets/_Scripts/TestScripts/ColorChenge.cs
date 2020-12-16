08c911dbb34716e6c2db84de4999b5bb86035ea9
高橋佑太 <yagisan555@icloud.com>
2020-12-16 19:29:37 +0900

    現状コミット

---
+++ Assets/_Scripts/TestScripts/ColorChenge.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChenge : MonoBehaviour
+{
+    public Material[] _material;
+    private int i;
+
+    private void Start()
+    {
+        i = 0;
+        //ColorChenge();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            i++;
+            //ColorChenge();
+            if(i == 2)
+            {
+                i = 0;
+            }
+            this.GetComponent<Renderer>().sharedMaterial = _material[i];
+        }
+    }
+
+    void Colorchenge()
+    {
+    }
+}
